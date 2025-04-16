2c43bfc94afb30cb0670f63df824d2f28e2986a5
Natalicio Borges Neto <168031076+natalicioneto@users.noreply.github.com>
2025-04-16 07:44:17 -0300

    Create otimizar_inicialização.ps1

---
+++ otimizar_inicialização.ps1
@@ -0,0 +1,7 @@
+# Mostra programas que iniciam com o Windows e permite desabilitar
+
+Write-Host "Listando aplicativos na inicialização..." -ForegroundColor Cyan
+$startupApps = Get-CimInstance Win32_StartupCommand | Select-Object Name, Command, Location
+$startupApps | Format-Table -AutoSize
+
+Write-Host "`nPara desabilitar programas na inicializaÃ§Ã£o, use o Gerenciador de Tarefas (Ctrl+Shift+Esc) > Inicializar." -ForegroundColor Yellow
